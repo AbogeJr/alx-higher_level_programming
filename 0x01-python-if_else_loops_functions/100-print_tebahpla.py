8b8e5759d8d52922f5d51724a655decb04b32546
AbogeJr <junioraboge@gmail.com>
2022-08-24 02:42:29 -0700

    added #12

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for c in reversed(range(97, 123)):
-    if c%2 != 0:
+    if c % 2 != 0:
         c -= 32
     print('{}'.format(chr(c)), end='')
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *ptr, *tmp, *new_node;
+
+	ptr = *head;
+	tmp = ptr->next;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if(!new_node)
+	{
+		printf("Allocation Error!\n");
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+	new_node->n = number;
+
+	while(ptr)
+	{
+		if(tmp->n >= number)
+			break;
+		ptr = tmp;
+		tmp = tmp->next;
+	}
+
+	new_node->next = tmp;
+	ptr->next = new_node;
+
+	return (new_node);
+}
