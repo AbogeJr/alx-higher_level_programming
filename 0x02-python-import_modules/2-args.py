50f25f477c597d07cdda07f1957aec4e489039f6
AbogeJr <junioraboge@gmail.com>
2022-08-25 02:19:19 -0700

    added files

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    from sys import argv
+
+    argc = len(argv)
+
+    if argc == 1:
+        print('0 arguments.')
+    elif argc == 2:
+        print('{} argument:'.format(argc-1))
+        print('1: {}'.format(argv[1]))
+    else:
+        print('{} arguments:'.format(argc-1))
+        for i in range(1, argc):
+            print('{0} : {1}'.format(i, argv[i]))
