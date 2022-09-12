09b798c60422199086f6d62c16dfbf301f52e096
AbogeJr <junioraboge@gmail.com>
2022-09-12 01:28:16 -0700

    added files

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    nb = 0
+    for i in range(x):
+        if type(my_list[i]) != int:
+            pass
+        else:
+            try:
+                print('{:d}'.format(my_list[i]), end='')
+                nb += 1
+            except Exception as err:
+                pass
+    print()
+    return nb
